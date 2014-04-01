586a8d70f58c6d71d5c12011982aa908af5258bf
Jonathan Ong <jonathanrichardong@gmail.com>
2014-03-31 21:25:56 -0700

    make more lib-ish

--- index.js
+++ index.js
@@ -4,58 +4,46 @@
  */
 
 var archy = require('archy');
-var program = require('commander');
-var utils = require('component-consoler');
-var resolve = require('component-resolver');
 
-// parse argv
-
-program
-  .option('-d, --depth <n>', 'set the depth to <n>', Infinity)
-  .parse(process.argv);
-
-// output tree
-
-resolve(process.cwd(), function (err, tree) {
-  if (err) utils.fatal(err);
+module.exports = function (tree, max) {
   console.log();
-  console.log(indent(archy(traverse(tree))));
-})
+  console.log(indent(archy(traverse(tree, 0))));
 
-/**
- * Return the dependency tree of the given config `file`.
- *
- * @param {String} file
- * @param {Array} paths
- * @return {Object}
- */
+  /**
+   * Return the dependency tree of the given config `file`.
+   *
+   * @param {String} file
+   * @param {Array} paths
+   * @return {Object}
+   */
 
-function traverse(branch, depth) {
-  depth = depth || 0;
+  function traverse(branch, depth) {
+    depth = depth || 0;
 
-  var color = !branch.parent
-    ? '\033[34m'
-    : branch.type === 'local'
-    ? '\033[35m'
-    : '\033[36m';
+    var color = !branch.parent
+      ? '\033[34m'
+      : branch.type === 'local'
+      ? '\033[35m'
+      : '\033[36m';
 
-  var node = {
-    label: color + branch.name + '\033[m'
-      + ' \033[90m' + (branch.version || branch.ref || '') + '\033[m',
-    nodes: []
-  };
+    var node = {
+      label: color + branch.name + '\033[m'
+        + ' \033[90m' + (branch.version || branch.ref || '') + '\033[m',
+      nodes: []
+    };
 
-  if (++depth > program.depth) return node;
+    if (++depth > max) return node;
 
-  Object.keys(branch.dependencies).forEach(function (name) {
-    node.nodes.push(traverse(branch.dependencies[name]));
-  });
+    Object.keys(branch.dependencies).forEach(function (name) {
+      node.nodes.push(traverse(branch.dependencies[name]));
+    });
 
-  Object.keys(branch.locals).forEach(function (name) {
-    node.nodes.push(traverse(branch.locals[name]));
-  });
+    Object.keys(branch.locals).forEach(function (name) {
+      node.nodes.push(traverse(branch.locals[name]));
+    });
 
-  return node;
+    return node;
+  }
 }
 
 /**
